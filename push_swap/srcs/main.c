836813ab66f11ecdfd237700fc71bd0f8d26a3f2
cw3l <cw3l@Mac.lan>
2024-11-22 18:18:17 +0100

    push_swap/Ven_22_nov_2024_18:18:17_CET

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,30 +6,54 @@
 /*   By: cw3l <cw3l@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 13:53:07 by cw3l              #+#    #+#             */
-/*   Updated: 2024/11/22 17:53:19 by cw3l             ###   ########.fr       */
+/*   Updated: 2024/11/22 18:17:57 by cw3l             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+int ft_print_error()
+{
+    write(1, "ERROR", 5);
+    return (1);
+}
+
+void    ft_print_arr(int *arr, int len)
+{
+    int i;
+
+    i = 0;
+    while (i < len)
+        printf("voici l'entier %d\n", arr[i++]);
+}
+
+int *ft_init_progr(char **argv)
+{
+    int len;
+    int *arr;
+    if(!ft_validation_arg(&argv[1]))
+    {
+        len = number_of_int(argv);
+        arr = ft_parsing(&argv[0], len);
+        int rep = ft_check_repetition(arr, len);
+        if(rep)
+            return(NULL);
+        else 
+            return(arr);
+    }
+    return (NULL);    
+}
+
 int main(int argc, char **argv)
 {
     int *i;
-    int j;
-    int len;
     
-    j = 0;
-     if(!ft_validation_arg(&argv[1]))
-     {
-        len = number_of_int(argv);
-        i = ft_parsing(&argv[1], len);
-        int rep = ft_check_repetition(i, len);
-        if(rep)
-            print_str("il y a une repetion");
-        else 
-            print_str("il n'y a pas de repetition");
-     }
+    if (argc < 2)
+        return (ft_print_error());
+    int *arr = ft_init_progr(&argv[1]);
+    if (!arr)
+        return (ft_print_error());
     else
-        print_str("ERROR\n");
+    
     return (0);
 }
